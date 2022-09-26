3135c67e0f24b9371f00b1490c5578fb0e05b628
bulidiriba <bulidiriba@gmail.com>
2022-09-27 00:30:21 +0300

    add product type and fetch data from fakestore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+import { Drawer, LinearProgress, Grid, Badge, } from "@mui/material";
+import { AddShoppingCart } from '@mui/icons-material';
+
+// types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+
+}
+
+
+const getProducts = async (): Promise<CartItemType[]> => 
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products", 
+    getProducts
+  );
+  console.log(isLoading);  
+  console.log(data);
+  console.log(error);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
